5890932061e6be7f1968ae6698e09b4a7966be3a
Jung Woo Sohn <70755880+sa03134@users.noreply.github.com>
2022-02-21 16:39:27 +0900

    first commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.example.myapplication
 
+import android.content.Context
 import android.os.Bundle
-import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityMainBinding
 
@@ -14,23 +15,8 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-//        binding.tv.text = "가즈아"
-//        binding.btnTest.text="보여?"
-
     }
 
-    var state = true
-    fun funTest(view: View) {
-
-        state = !state
-        if (state) {
-            binding.tv.text = "true"
-            binding.tvSub.text ="해냈다"
-        } else {
-            binding.tv.text = "false"
-            binding.tvSub.text ="요롤레이 요호호"
-        }
-    }
 
 }
 
